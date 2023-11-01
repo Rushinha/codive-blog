24a7adcc333b054bda45240a8ec479acf76bf354
Josias Junior <1001josias@gmail.com>
2023-11-01 17:54:42 -0300

    contentlayer config

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,8 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import { s } from "hastscript";
 
 export const Post = defineDocumentType(() => ({
   name: "Post",
@@ -7,6 +11,15 @@ export const Post = defineDocumentType(() => ({
   fields: {
     title: { type: "string", required: true },
     date: { type: "date", required: true },
+    excerpt: {
+      type: "string",
+      required: true,
+    },
+    tags: {
+      type: "list",
+      of: { type: "string" },
+      required: true,
+    },
   },
   computedFields: {
     url: {
@@ -19,4 +32,26 @@ export const Post = defineDocumentType(() => ({
 export default makeSource({
   contentDirPath: "src/posts",
   documentTypes: [Post],
+  mdx: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+          properties: { class: "heading-link" },
+          content: s("span", "#"),
+        },
+      ],
+      [
+        rehypePrettyCode,
+        {
+          theme: {
+            // light: 'github-light',
+            dark: "github-dark",
+          },
+        },
+      ],
+    ],
+  },
 });
--- src/app/(posts)/[slug]/page.tsx
+++ src/app/(posts)/[slug]/page.tsx
@@ -11,12 +11,13 @@ export const generateMetadata = ({ params }: { params: { slug: string } }) => {
   return { title: post.title };
 };
 
-const PostLayout = ({ params }: { params: { slug: string } }) => {
+export default function PostLayout({ params }: { params: { slug: string } }) {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
   if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
   const MdxComponent = useMDXComponent(post.body.code);
+
   return (
-    <article className='mx-auto max-w-xl py-8'>
+    <article className='prose dark:prose-invert mx-auto max-w-xl py-8'>
       <div className='mb-8 text-center'>
         <time dateTime={post.date} className='mb-1 text-xs text-gray-600'>
           {format(parseISO(post.date), "LLLL d, yyyy")}
@@ -28,6 +29,4 @@ const PostLayout = ({ params }: { params: { slug: string } }) => {
       </div>
     </article>
   );
-};
-
-export default PostLayout;
+}
